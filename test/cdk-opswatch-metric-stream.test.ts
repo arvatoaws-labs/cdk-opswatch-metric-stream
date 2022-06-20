e7e5b737a2b17c6aecbea353d373f00567990466
Patrick Robinson <patrick.robinson@bertelsmann.de>
2022-06-20 13:46:47 +0200

    Initial commit

---
+++ test/cdk-opswatch-metric-stream.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkOpswatchMetricStream from '../lib/cdk-opswatch-metric-stream-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-opswatch-metric-stream-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkOpswatchMetricStream.CdkOpswatchMetricStreamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
